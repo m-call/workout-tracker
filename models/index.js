0e850fdd672b3cdeb564abf89bce75b33d8eca05
Michael Callahan <mcallahan0@yahoo.com>
2021-08-12 19:08:28 -0400

    Changed index.js to only include a module.exports for the Workout model from the Workout.js file

--- models/index.js
+++ models/index.js
@@ -1,8 +1,3 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const Workout = mongoose.model("Workout", WorkoutSchema);
-
-const db = {Workout: Workout};
-
-module.exports = db;
+module.exports = {
+    Workout: require('./Workout')
+};
