218acfe528ab614573efa48d19046d6451e85176
Michael Callahan <mcallahan0@yahoo.com>
2021-08-12 19:25:45 -0400

    Updated index.js in routes folder

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const workoutRoutes = require('./workoutRoutes');
 const htmlRoutes = require('./htmlRoutes');
 
