a1c6d0762756a603059b362eae05b9da54215505
Michael Callahan <mcallahan0@yahoo.com>
2021-08-12 17:52:16 -0400

    Created an api folder inside the routes folder

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-// const statsRoutes = require('../public/stats');
+const apiRoutes = require('./api');
 
-// router.use('/stats', statsRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
