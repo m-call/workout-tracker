52bf2637155317f08178b4c9af54d173f654e7a2
Michael Callahan <mcallahan0@yahoo.com>
2021-08-12 20:47:09 -0400

    Added and implemented the get request for the workouts range which aggregates the total duration and total weight over the past 7 days

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -11,6 +11,28 @@ router.get('/workouts', (req, res) => {
         });
 });
 
+router.get('/workouts/range', (req, res) => {
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                },
+                totalWeight: {
+                    $sum: "exercises.weight"
+                }
+            }
+        }
+    ])
+    .limit(7)
+    .then(response => {
+        res.status(200).json(response);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
 router.post('/workouts', ({ body }, res) => {
     db.Workout.create(body)
         .then (response => {
